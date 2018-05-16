0b6521394865fcf43e467813263fa43c7df034ec
robotemperor <leeche12@hanyang.ac.kr>
2018-05-16 22:03:55 +0900

    base_module

---
+++ alice_base_module/include/alice_base_module/base_module.h
@@ -0,0 +1,82 @@
+/*
+ * base_module.h
+ *
+ *  Created on: May 16, 2018
+ *      Author: robotemperor
+ */
+
+#ifndef HEROEHS_ALICE_MOTION_ALICE_BASE_MODULE_INCLUDE_ALICE_BASE_MODULE_BASE_MODULE_H_
+#define HEROEHS_ALICE_MOTION_ALICE_BASE_MODULE_INCLUDE_ALICE_BASE_MODULE_BASE_MODULE_H_
+
+#include <map>
+#include <ros/ros.h>
+#include <ros/callback_queue.h>
+#include <ros/package.h>
+#include <std_msgs/Int16.h>
+#include <std_msgs/Float64.h>
+#include <std_msgs/String.h>
+#include <geometry_msgs/Pose.h>
+#include <boost/thread.hpp>
+
+#include <Eigen/Dense>
+#include <yaml-cpp/yaml.h>
+#include <fstream>
+#include <stdio.h>
+
+#include "robotis_framework_common/motion_module.h"
+#include "robotis_controller_msgs/StatusMsg.h"
+#include "robotis_math/robotis_math.h"
+
+#include "heroehs_math/fifth_order_trajectory_generate.h"
+#include "heroehs_math/kinematics.h"
+
+namespace alice
+{
+
+
+class BaseModule: public robotis_framework::MotionModule, public robotis_framework::Singleton<BaseModule>
+{
+public:
+	BaseModule();
+	virtual ~BaseModule();
+
+	/* ROS Framework Functions */
+	void initialize(const int control_cycle_msec, robotis_framework::Robot *robot);
+	void process(std::map<std::string, robotis_framework::Dynamixel *> dxls, std::map<std::string, double> sensors);
+
+	void stop();
+	bool isRunning();
+
+	bool gazebo_check;
+
+	/* ROS Topic Callback Functions */
+
+	void initPoseMsgCallback(const std_msgs::String::ConstPtr& msg);
+	std::map<std::string, heroehs_math::FifthOrderTrajectory *> motion_trajectory;
+
+private:
+	void queueThread();
+	void parse_init_pose_data_(const std::string &path);
+	void init_pose_trajectory_();
+	bool running_;
+
+	int new_count_;
+	int control_cycle_msec_;
+
+	boost::thread queue_thread_;
+
+	std::map<std::string, int> joint_name_to_id_;
+	std::map<int, std::string> joint_id_to_name_;
+
+	bool is_moving_state;
+	double mov_time_state;
+	std::map<std::string, double> joint_name_to_ini_pose_state_;
+	std::map<std::string, double> joint_name_to_ini_pose_goal_;
+};
+
+
+}
+
+
+
+#endif /* HEROEHS_ALICE_MOTION_ALICE_BASE_MODULE_INCLUDE_ALICE_BASE_MODULE_BASE_MODULE_H_ */
---
+++ alice_base_module/src/base_module.cpp
@@ -0,0 +1,220 @@
+/*
+ * base_module.cpp
+ *
+ *  Created on: May 16, 2018
+ *      Author: robotemperor
+ */
+
+/*
+ * base_module.cpp
+ *
+ *  Created on: 2017. 10. 14.
+ *      Author: robotemperor
+ */
+#include <stdio.h>
+#include "alice_base_module/base_module.h"
+using namespace alice;
+
+BaseModule::BaseModule()
+: control_cycle_msec_(8)
+{
+	running_ = false;
+	enable_       = false;
+	gazebo_check  = false;
+	module_name_  = "base_module";
+	control_mode_ = robotis_framework::PositionControl;
+
+	// Dynamixel initialize ////
+	/*
+	result_["l_shoulder_pitch"] = new robotis_framework::DynamixelState();  // joint 1
+	result_["r_shoulder_pitch"] = new robotis_framework::DynamixelState();  // joint 2
+	result_["l_shoulder_roll"]  = new robotis_framework::DynamixelState();  // joint 3
+
+	result_["r_shoulder_roll"]  = new robotis_framework::DynamixelState();  // joint 4
+	result_["l_elbow_pitch"]    = new robotis_framework::DynamixelState();  // joint 5
+	result_["r_elbow_pitch"]    = new robotis_framework::DynamixelState();  // joint 6
+
+	result_["waist_yaw"]        = new robotis_framework::DynamixelState();  // joint 9
+	result_["waist_roll"]       = new robotis_framework::DynamixelState();  // joint 10
+
+	result_["l_hip_pitch"]      = new robotis_framework::DynamixelState();  // joint 11
+	result_["l_hip_roll"]       = new robotis_framework::DynamixelState();  // joint 13
+	result_["l_hip_yaw"]        = new robotis_framework::DynamixelState();  // joint 15
+	result_["l_knee_pitch"]     = new robotis_framework::DynamixelState();  // joint 17
+	result_["l_ankle_pitch"]    = new robotis_framework::DynamixelState();  // joint 19
+	result_["l_ankle_roll"]     = new robotis_framework::DynamixelState();  // joint 21
+
+	result_["r_hip_pitch"]      = new robotis_framework::DynamixelState();  // joint 12
+	result_["r_hip_roll"]       = new robotis_framework::DynamixelState();  // joint 14
+	result_["r_hip_yaw"]        = new robotis_framework::DynamixelState();  // joint 16
+	result_["r_knee_pitch"]     = new robotis_framework::DynamixelState();  // joint 18
+	result_["r_ankle_pitch"]    = new robotis_framework::DynamixelState();  // joint 20
+	result_["r_ankle_roll"]     = new robotis_framework::DynamixelState();  // joint 22
+
+	result_["head_yaw"]         = new robotis_framework::DynamixelState();  // joint 23
+	result_["head_pitch"]       = new robotis_framework::DynamixelState();  // joint 24
+	result_["head_roll"]        = new robotis_framework::DynamixelState();  // joint 25
+	 */
+
+	// TEST
+	result_["l_hip_pitch"]     = new robotis_framework::DynamixelState();  // joint 17
+	result_["r_hip_pitch"]     = new robotis_framework::DynamixelState();  // joint 18
+
+	//init
+	new_count_ = 1;
+	is_moving_state = false;
+	mov_time_state = 0;
+
+	joint_name_to_ini_pose_state_.clear();
+	joint_name_to_ini_pose_goal_.clear();
+	joint_name_to_id_.clear();
+	joint_id_to_name_.clear();
+}
+BaseModule::~BaseModule()
+{
+	queue_thread_.join();
+}
+void BaseModule::initialize(const int control_cycle_msec, robotis_framework::Robot *robot)
+{
+	control_cycle_msec_ = control_cycle_msec;
+	queue_thread_ = boost::thread(boost::bind(&BaseModule::queueThread, this));
+
+	for (std::map<std::string, robotis_framework::Dynamixel*>::iterator it = robot->dxls_.begin();
+			it != robot->dxls_.end(); it++)
+	{
+		std::string joint_name = it->first;
+		robotis_framework::Dynamixel* dxl_info = it->second;
+
+		joint_name_to_id_[joint_name] = dxl_info->id_;
+		joint_id_to_name_[dxl_info->id_] = joint_name;
+		motion_trajectory[joint_name] = new heroehs_math::FifthOrderTrajectory();
+	}
+	ROS_INFO("< -------  Initialize Module : Base Module !!  ------->");
+}
+
+void BaseModule::parse_init_pose_data_(const std::string &path)
+{
+	YAML::Node doc; // YAML file class 선언!
+	try
+	{
+		// load yaml
+		doc = YAML::LoadFile(path.c_str()); // 파일 경로를 입력하여 파일을 로드 한다.
+
+	}catch(const std::exception& e) // 에러 점검
+	{
+		ROS_ERROR("Fail to load yaml file!");
+		return;
+	}
+	double mov_time_ = 0.0;
+
+	mov_time_= doc["mov_time"].as<double>(); // YAML 에 string "mov_time"을 읽어온다.
+	mov_time_state = mov_time_;
+
+	YAML::Node tar_pose_node = doc["tar_pose"];// YAML 에 string "tar_pose"을 읽어온다.
+	for (YAML::iterator it = tar_pose_node.begin(); it != tar_pose_node.end(); ++it) //tar_pose_node 벡터의 처음과 시작 끝까지 for 문으로 반복한다.
+	{
+		int id;
+		double value;
+		// 한 줄에서 int 와 double 을 분리한다.
+		id = it->first.as<int>();
+		value = it->second.as<double>();
+		joint_name_to_ini_pose_goal_[joint_id_to_name_[id]] = value * DEGREE2RADIAN; // YAML에 로드된 초기 포즈 값을 라디안으로 바꾸고, eigen matrix 에 id 개수만큼 열을 생성한다.
+	}
+}
+void BaseModule::initPoseMsgCallback(const std_msgs::String::ConstPtr& msg) // GUI 에서 init pose topic을 sub 받아 실
+{
+	std::string init_pose_path;// 로스 패키지에서 YAML파일의 경로를 읽어온다.
+	printf("%s", msg->data.c_str());
+	if(msg->data.compare("init_pose") == 0)
+	{
+		init_pose_path = ros::package::getPath("alice_base_module") + "/data/init_pose.yaml";// 로스 패키지에서 YAML파일의 경로를 읽어온다.
+		ROS_INFO("FILE LOAD  ::  init_pose");
+	}
+	else
+		return;
+
+	parse_init_pose_data_(init_pose_path); // YAML 파일 로드
+	is_moving_state = true;
+	ROS_INFO("FILE LOAD complete");
+}
+void BaseModule::queueThread()
+{
+	ros::NodeHandle ros_node;
+	ros::CallbackQueue callback_queue;
+
+	ros_node.setCallbackQueue(&callback_queue);
+	/* subscribe topics */
+	// for gui
+	ros::Subscriber ini_pose_msg_sub = ros_node.subscribe("/init_pose", 5, &BaseModule::initPoseMsgCallback, this);
+	ros::WallDuration duration(control_cycle_msec_ / 1000.0);
+
+	while(ros_node.ok())
+		callback_queue.callAvailable(duration);
+}
+bool BaseModule::isRunning()
+{
+	return running_;
+}
+void BaseModule::process(std::map<std::string, robotis_framework::Dynamixel *> dxls,
+		std::map<std::string, double> sensors)
+{
+	if (enable_ == false)
+	{
+		return;
+	}
+
+	//// read current position ////
+	if(new_count_ == 1)
+	{
+		ROS_INFO("Base_Process Start!");
+		new_count_ ++;
+
+		for (std::map<std::string, robotis_framework::Dynamixel*>::iterator state_iter = dxls.begin();
+				state_iter != dxls.end(); state_iter++)
+		{
+			std::string joint_name = state_iter->first;
+			if(gazebo_check == true)
+				result_[joint_name]->goal_position_ = result_[joint_name]->present_position_; // 가제보 상 초기위치 0
+			else
+				result_[joint_name]->goal_position_ = dxls[joint_name]->dxl_state_->present_position_; // 다이나믹셀에서 읽어옴
+			joint_name_to_ini_pose_state_[joint_name] = dxls[joint_name]->dxl_state_->present_position_; // 초기위치 저장
+		} // 등록된 다이나믹셀의 위치값을 읽어와서 goal position 으로 입력
+		ROS_INFO("Base module :: Read position and Send goal position");
+	}
+	// trajectory is working joint space control
+	if(is_moving_state == false)
+	{
+		ROS_INFO("Base Stay");
+		for (std::map<std::string, robotis_framework::Dynamixel*>::iterator state_iter = dxls.begin();
+				state_iter != dxls.end(); state_iter++)
+		{
+			std::string joint_name = state_iter->first;
+
+			dxls[joint_name]->dxl_state_->present_position_;
+		} // 등록된 다이나믹셀  goal position 으로 입력
+	}
+	else
+	{
+		ROS_INFO("Base Trajectory Start");
+		for (std::map<std::string, robotis_framework::Dynamixel*>::iterator state_iter = dxls.begin();
+				state_iter != dxls.end(); state_iter++)
+		{
+			std::string joint_name = state_iter->first;
+
+			result_[joint_name]->goal_position_ =  motion_trajectory[joint_name]->fifth_order_traj_gen(joint_name_to_ini_pose_state_[joint_name],
+					joint_name_to_ini_pose_goal_[joint_name],0,0,0,0,0,mov_time_state);
+
+
+
+			is_moving_state = motion_trajectory[joint_name]->is_moving_traj;
+		} // 등록된 다이나믹셀  goal position 으로 입력
+	}
+}
+void BaseModule::stop()
+{
+	return;
+}
+
+
+
+
