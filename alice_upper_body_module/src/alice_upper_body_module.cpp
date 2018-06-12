7208df11fd9d53904fb61d32232b70621a15b8c6
robotemperor <leeche12@hanyang.ac.kr>
2018-06-12 18:00:00 +0900

    m

--- alice_upper_body_module/src/alice_upper_body_module.cpp
+++ alice_upper_body_module/src/alice_upper_body_module.cpp
@@ -231,18 +231,21 @@ void UpperBodyModule::tracking_function()
 	control_angle_yaw   = pidController_x->PID_calculate(desired_x, current_x);
 	control_angle_pitch = pidController_y->PID_calculate(desired_y, current_y);
 
-	control_angle_yaw   += control_angle_yaw;
+	printf("X   control value ::  %f \n",current_x);
+	printf("Y   control value ::  %f \n",current_y);
+
+/*	control_angle_yaw   += control_angle_yaw;
 	control_angle_pitch = -control_angle_pitch;
 
 	control_angle_pitch += control_angle_pitch + 35*DEGREE2RADIAN;
 
 	head_end_point_(3, 1)  = control_angle_yaw;
-	head_end_point_(4, 1)  = control_angle_pitch;
+	head_end_point_(4, 1)  = control_angle_pitch;*/
 	//head_end_point_(3, 7)  = 0.8;
 	//head_end_point_(4, 7)  = 0.8;
 
-	printf("yaw   control value ::  %f \n",control_angle_yaw);
-	printf("pitch control value ::  %f \n",control_angle_pitch);
+	//printf("yaw   control value ::  %f \n",control_angle_yaw);
+	//printf("pitch control value ::  %f \n",control_angle_pitch);
 	pre_current_x = current_x;
 	pre_current_y = current_y;
 
